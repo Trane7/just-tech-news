bf995c1234c9d9beb6d2e98b8d9bbb56c33e1951
Dee <dgrowette@gmail.com>
2022-01-07 21:08:13 -0800

    .env user n pw change

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 // create connection to our database, pass in your MySQL information for username and password
+console.log(process.env);
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
